ad6942686933283808caf3117d8d281ed3ef8c43
Jonny Burger <jonathanburger11@gmail.com>
2022-07-08 21:39:40 +0200

    draw an oval

--- src/HelloSkia.tsx
+++ src/HelloSkia.tsx
@@ -1,13 +1,23 @@
 import {SkiaCanvas} from '@remotion/skia';
-import {Circle} from '@shopify/react-native-skia';
+import {Oval} from '@shopify/react-native-skia';
 import {useVideoConfig} from 'remotion';
+
+const rx = 400;
+const ry = 200;
 
 export const HelloSkia: React.FC = () => {
 	const {height, width} = useVideoConfig();
 	// A <AbsoluteFill> is just a absolutely positioned <div>!
 	return (
 		<SkiaCanvas height={height} width={width}>
-			<Circle cx={width / 2} cy={height / 2} r={100} color="red" />
+			<Oval
+				x={width / 2 - rx / 2}
+				y={height / 2 - ry / 2}
+				width={rx}
+				height={ry}
+				style="stroke"
+				strokeWidth={30}
+			/>
 		</SkiaCanvas>
 	);
 };
