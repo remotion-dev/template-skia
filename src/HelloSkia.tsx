29f3d0ed34c6caeeca5051e223e8cd46be60cb9e
Jonny Burger <jonathanburger11@gmail.com>
2022-07-12 11:29:34 +0200

    update with william's best practices

--- src/HelloSkia.tsx
+++ src/HelloSkia.tsx
@@ -1,40 +1,16 @@
 import {SkiaCanvas} from '@remotion/skia';
-import {
-	Circle,
-	Fill,
-	Group,
-	Mask,
-	Oval,
-	Shader,
-	Skia,
-	SkRuntimeEffect,
-	useFont,
-	Text,
-} from '@shopify/react-native-skia';
-import {spring, staticFile, useCurrentFrame, useVideoConfig} from 'remotion';
-import {notargs} from './shaders/notargs';
-
-const rx = 600 * 0.8;
-const ry = 250 * 0.8;
-
-const shader = Skia.RuntimeEffect.Make(notargs);
+import {useFont} from '@shopify/react-native-skia';
+import {staticFile, useVideoConfig} from 'remotion';
+import {AssetManager} from './AssetManager';
+import {Drawing} from './Drawing';
 
 const roboto = staticFile('Roboto-Bold.ttf');
 
 export const HelloSkia: React.FC = () => {
-	const {height, width, fps} = useVideoConfig();
-	const frame = useCurrentFrame();
+	const {height, width} = useVideoConfig();
 
 	const bigFont = useFont(roboto, 64);
 	const smallFont = useFont(roboto, 30);
-
-	const progress = spring({
-		fps,
-		frame,
-		config: {
-			damping: 200,
-		},
-	});
 
 	if (bigFont === null || smallFont === null) {
 		return null;
@@ -42,65 +18,14 @@ export const HelloSkia: React.FC = () => {
 
 	return (
 		<SkiaCanvas height={height} width={width}>
-			<Fill color="white" />
-			<Mask
-				mask={
-					<Group
-						transform={[
-							{
-								translateY: -100,
-							},
-						]}
-					>
-						{new Array(3).fill(true).map((f, i) => {
-							return (
-								<Group
-									key={i}
-									origin={{
-										x: width / 2,
-										y: height / 2,
-									}}
-									transform={[
-										{
-											rotate: (Math.PI / 3) * i * progress,
-										},
-									]}
-								>
-									<Oval
-										width={rx}
-										height={ry}
-										style="stroke"
-										strokeWidth={35}
-										x={width / 2 - rx / 2}
-										y={height / 2 - ry / 2}
-									/>
-								</Group>
-							);
-						})}
-						<Circle cx={width / 2} cy={height / 2} r={40} />
-					</Group>
-				}
+			<AssetManager
+				images={[]}
+				typefaces={{
+					Roboto: roboto,
+				}}
 			>
-				<Fill>
-					<Shader
-						source={shader as SkRuntimeEffect}
-						uniforms={{iTime: frame / fps, iWidth: width}}
-					/>
-				</Fill>
-			</Mask>
-			<Text
-				x={width / 2 - 150}
-				y={height / 2 + 240}
-				font={bigFont}
-				text="Hello Skia!"
-				size={64}
-			/>
-			<Text
-				x={width / 2 - 270}
-				y={height / 2 + 300}
-				font={smallFont}
-				text="Edit src/HelloSkia.tsx and save to refresh."
-			/>
+				<Drawing />
+			</AssetManager>
 		</SkiaCanvas>
 	);
 };
