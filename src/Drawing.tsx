ac19a7ac665a9548febfba85b87909def3919aaf
William Candillon <wcandillon@gmail.com>
2022-07-20 16:06:58 +0200

    Cosmetic improvements

--- src/Drawing.tsx
+++ src/Drawing.tsx
@@ -12,13 +12,13 @@ import {
 } from '@shopify/react-native-skia';
 import React from 'react';
 import {useCurrentFrame, useVideoConfig} from 'remotion';
-import {notargs} from './shaders/notargs';
 import {useTypefaces} from './AssetManager';
+import { xordev } from './shaders/xordev';
 
 const rx = 600 * 0.8;
 const ry = 250 * 0.8;
 
-const shader = Skia.RuntimeEffect.Make(notargs);
+const shader = Skia.RuntimeEffect.Make(xordev);
 
 export const Drawing: React.FC = () => {
 	const frame = useCurrentFrame();
@@ -37,63 +37,27 @@ export const Drawing: React.FC = () => {
 
 	return (
 		<>
-			<Fill color="white" />
-			<Mask
-				mask={
-					<Group
-						transform={[
-							{
-								translateY: -100,
-							},
-						]}
-					>
-						{new Array(3).fill(true).map((f, i) => {
-							return (
-								<Group
-									key={i}
-									origin={{
-										x: width / 2,
-										y: height / 2,
-									}}
-									transform={[
-										{
-											rotate: (Math.PI / 3) * i * progress,
-										},
-									]}
-								>
-									<Oval
-										width={rx}
-										height={ry}
-										style="stroke"
-										strokeWidth={35}
-										x={width / 2 - rx / 2}
-										y={height / 2 - ry / 2}
-									/>
-								</Group>
-							);
-						})}
-						<Circle cx={width / 2} cy={height / 2} r={40} />
-					</Group>
-				}
-			>
-				<Fill>
-					<Shader
-						source={shader as SkRuntimeEffect}
-						uniforms={{iTime: frame / fps, iWidth: width}}
-					/>
-				</Fill>
-			</Mask>
+			<Fill color="black" />
+
+			<Fill>
+				<Shader
+					source={shader as SkRuntimeEffect}
+					uniforms={{iTime: frame / fps, iResolution: [width, height*0.75]}}
+				/>
+			</Fill>
 			<Text
 				x={width / 2 - 150}
 				y={height / 2 + 240}
 				font={bigFont}
 				text="Hello Skia!"
+				color="white"
 			/>
 			<Text
 				x={width / 2 - 270}
 				y={height / 2 + 300}
 				font={smallFont}
 				text="Edit src/HelloSkia.tsx and save to refresh."
+				color="white"
 			/>
 		</>
 	);
---
+++ src/shaders/xordev.ts
@@ -0,0 +1,18 @@
+// Source: @XorDev https://twitter.com/XorDev/status/1475524322785640455
+export const xordev = `
+uniform float iTime;
+uniform float2 iResolution;
+
+vec4 main(vec2 FC) {
+  vec4 o = vec4(0);
+  vec2 p = vec2(0), c=p, u=FC.xy*2.-iResolution.xy;
+  float a;
+  for (float i=0; i<4e2; i++) {
+    a = i/2e2-1.;
+    p = cos(i*2.4+iTime+vec2(0,11))*sqrt(1.-a*a);
+    c = u/iResolution.y+vec2(p.x,a)/(p.y+2.);
+    o += (cos(i+vec4(0,2,4,0))+1.)/dot(c,c)*(1.-p.y)/3e4;
+  }
+  return o;
+}
+`;
