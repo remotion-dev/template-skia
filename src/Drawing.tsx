f16e1ac9d464bf7adf33cce6fe9d3a34745c6926
William Candillon <wcandillon@gmail.com>
2022-07-21 11:40:46 +0200

    Update example

--- src/Drawing.tsx
+++ src/Drawing.tsx
@@ -7,38 +7,22 @@ import {
 import React from 'react';
 import {useCurrentFrame, useVideoConfig} from 'remotion';
 import {useTypefaces} from './AssetManager';
-import { xordev } from './shaders/xordev';
+import { SkiaNeon } from './SkiaNeon';
 
 
-const shader = Skia.RuntimeEffect.Make(xordev)!;
+
 
 export const Drawing: React.FC = () => {
-	const frame = useCurrentFrame();
-	const {width, height, fps} = useVideoConfig();
+	const {width, height} = useVideoConfig();
 	const typefaces = useTypefaces();
 	const smallFont = Skia.Font(typefaces.Roboto, 30);
-	const bigFont = Skia.Font(typefaces.Roboto, 64);
-
 	return (
 		<>
 			<Fill color="black" />
-
-			<Fill>
-				<Shader
-					source={shader}
-					uniforms={{iTime: frame / fps, iResolution: [width, height*0.75], base: [4,4,0,0]}}
-				/>
-			</Fill>
-			<Text
-				x={width / 2 - 150}
-				y={height / 2 + 240}
-				font={bigFont}
-				text="Hello Skia!"
-				color="white"
-			/>
+			<SkiaNeon />
 			<Text
 				x={width / 2 - 270}
-				y={height / 2 + 300}
+				y={height * 0.9}
 				font={smallFont}
 				text="Edit src/HelloSkia.tsx and save to refresh."
 				color="white"
