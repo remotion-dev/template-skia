d6b3f0ac4acd484596390784524cff0c762eebb1
Jonny Burger <jonathanburger11@gmail.com>
2023-02-03 10:56:54 +0100

    new config file format

--- remotion.config.ts
+++ remotion.config.ts
@@ -5,12 +5,12 @@
 import {enableSkia} from '@remotion/skia/enable';
 import {Config} from 'remotion';
 
-Config.Rendering.setImageFormat('jpeg');
-Config.Output.setOverwriteOutput(true);
+Config.setImageFormat('jpeg');
+Config.setOverwriteOutput(true);
 
-Config.Bundling.overrideWebpackConfig((config) => {
+Config.overrideWebpackConfig((config) => {
 	return enableSkia(config);
 });
 
-Config.Rendering.setConcurrency(2);
-Config.Puppeteer.setChromiumOpenGlRenderer('angle');
+Config.setConcurrency(2);
+Config.setChromiumOpenGlRenderer('angle');
